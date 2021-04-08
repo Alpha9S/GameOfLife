dd2a590bdda5bfec4ef5aa27e0da7f9e4ab4b5bc
Hikmet Özer <hikmetozer@rocketmail.com>
2021-04-08 16:39:53 +0200

    Hinzufügen einer neuen Zellen-Klasse mit Konstruktor

---
+++ Game_of_Life/Game_of_Life/cell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_of_Life
+{
+    class cell
+    {
+        private int x, y;
+        public cell(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
