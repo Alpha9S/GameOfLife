8bc2ed6ed2f7c1e481218c05c34c3cbf86e814de
Hikmet Özer <hikmetozer@rocketmail.com>
2021-04-08 16:47:20 +0200

    Erneutes Hinzufügen der Cell-Klasse mit groß geschriebenem Namen

---
+++ Game_of_Life/Game_of_Life/Cell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_of_Life
+{
+    class Cell
+    {
+        private int x, y;
+        public Cell(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
