113e7014ec75c65d5b1fc292b067d2ad6ecd079b
Hikmet Özer <hikmetozer@rocketmail.com>
2021-04-15 12:39:24 +0200

    vollständiges füllen des Arrays mit den Zellen und deren Koordinaten, sowie Anpassung des Grid-Codes Außerdem umstellung von private auf public der Cell Eigenschaften x und y

--- Game_of_Life/Game_of_Life/Cell.cs
+++ Game_of_Life/Game_of_Life/Cell.cs
@@ -8,7 +8,7 @@ namespace Game_of_Life
 {
     class Cell
     {
-        private int x, y;
+        public int x, y;
         public Cell(int x, int y)
         {
             this.x = x;
--- Game_of_Life/Game_of_Life/MainWindow.xaml.cs
+++ Game_of_Life/Game_of_Life/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -11,27 +12,53 @@ namespace Game_of_Life
     {
         int gridColumns = 9;
         int gridRows = 9;
-
+        
+        // create an array for the coordinates of the cells
         private Cell[][] cells = new Cell[9][];
 
         public MainWindow()
         {
-            Grid grid = new Grid();
             InitializeComponent();
 
-
-
-            for(int i = 0; i < gridColumns; i++)
+            // Fill in the coordinates of the cells into the array
+            for (int x = 0; x < gridRows; x++)
             {
-                cells[i] = new Cell[9];
+                cells[x] = new Cell[9];
 
-                for (int x = 0; x < gridRows; x++)
+                for (int y = 0; y < gridRows; y++)
                 {
-                    cells[i][x] = new Cell(x, i);
+                    cells[x][y] = new Cell(x, y);
                 }
             }
 
-            gameboard.Background = new SolidColorBrush(Color.FromRgb(50, 100, 150));
+            // create a new grid
+            Grid grid = new Grid();
+            // zeige Trennlinien beim Grid
+            grid.ShowGridLines = true;
+            // Setze Hintergrundfarbe
+            grid.Background = new SolidColorBrush(Color.FromRgb(50, 100, 150));
+
+            // create columns for the grid
+            for(int i = 0; i < gridColumns; i++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            // create rows for the grid
+            for (int i = 0; i < gridRows; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition());
+            }
+
+            for (int x = 0; x < gridColumns; x++)
+            {
+                for (int y = 0; y < gridRows; y++)
+                {
+                    Console.WriteLine("X: " + cells[x][y].x + " Y: " + cells[x][y].y);
+                }
+            }
+
+            
         }
     }
 }
