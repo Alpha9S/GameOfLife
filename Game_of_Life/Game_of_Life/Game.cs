2167b761e33e7113bfab7eb6eb712ce45f6b0384
Hikmet Özer <hikmetozer@rocketmail.com>
2021-05-20 16:56:33 +0200

    Update vom 20.05.21 - Erstellung einer Game-Klasse, welche einige Funktionen übernommen hat - Gleich bleibende Funktionalität nach der Umverlegung und Kürzung vom vorhandenen Code

---
+++ Game_of_Life/Game_of_Life/Game.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Game_of_Life
+{
+    class Game
+    {
+        private DispatcherTimer timer = new DispatcherTimer();
+        private Random random = new Random();
+        private int gridRows;
+        private int gridColumns;
+        private Cell[][] cells;
+        private Grid grid;
+
+        public Game(int gridRows, int gridColumns)
+        {
+            this.gridRows = gridRows;
+            this.gridColumns = gridColumns;
+        }
+
+        public Grid Grid
+        {
+            get { return this.grid; }
+            set { this.grid = value; }
+        }
+        public void Game_Tick(object sender, EventArgs e)
+        {
+            Random_Cell_Generation();
+            Draw_Circles();
+        }
+
+        public void Game_Start()
+        {
+        }
+
+        public void Game_Stop()
+        {
+        }
+        public void Draw_Circles()
+        {
+            for(int x = 0; x < gridRows; x++)
+            {
+                for(int y = 0; y < this.gridColumns; y++)
+                {
+                    if (this.cells[x][y].Condition)
+                    {
+                        this.cells[x][y].Circle.Fill = Brushes.Black;
+                    }
+                    else
+                    {
+                        this.cells[x][y].Circle.Fill = Brushes.White;
+                    }
+                }
+            }
+        }
+        public void Random_Cell_Generation()
+        {
+            for(int x = 0; x < this.gridRows; x++)
+            {
+                for(int y = 0; y < this.gridColumns; y++)
+                {
+                    if(random.Next(2) == 1)
+                    {
+                        this.cells[x][y].Condition = true;
+                    }
+                    else
+                    {
+                        this.cells[x][y].Condition = false;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates cell objects for every cell inside the grid
+        /// </summary>
+        /// <param name="grid">The grid object</param>
+        /// <returns>Multidimensional array with cell objects inside</returns>
+        public void Cell_Generation()
+        {
+            Cell[][] cells = new Cell[this.gridRows][];
+
+            for (int x = 0; x < this.gridRows; x++)
+            {
+                cells[x] = new Cell[this.gridColumns];
+
+                for (int y = 0; y < this.gridColumns; y++)
+                {
+                    cells[x][y] = new Cell(x, y, true);
+                    Border border = new Border();
+                    Ellipse circle = new Ellipse();
+
+                    // add border and circle to the grid children
+                    grid.Children.Add(border);
+                    grid.Children.Add(circle);
+
+                    // set x and y coordinates for the border
+                    border.SetValue(Grid.RowProperty, x);
+                    border.SetValue(Grid.ColumnProperty, y);
+                    // cell border color and thickness
+                    border.BorderBrush = Brushes.Black;
+                    border.BorderThickness = new Thickness(1);
+
+                    // set x and y coordinates for the circle
+                    circle.SetValue(Grid.RowProperty, x);
+                    circle.SetValue(Grid.ColumnProperty, y);
+                    // margin between circle and cell border
+                    circle.Margin = new Thickness(8);
+
+                    // hands over the circle object to the cell object
+                    cells[x][y].Circle = circle;
+                }
+            }
+
+            this.cells = cells;
+        }
+    }
+}
--- Game_of_Life/Game_of_Life/MainWindow.xaml.cs
+++ Game_of_Life/Game_of_Life/MainWindow.xaml.cs
@@ -6,7 +6,7 @@ using System.Windows.Media;
 using System.Windows.Shapes;
 
 /// <summary>
-/// Version 0.1
+/// Version 0.2
 /// Author: Hikmet Özer
 /// Author: Leon Hoppe
 /// </summary>
@@ -17,21 +17,18 @@ namespace Game_of_Life
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static int gridColumns = 11;
+        private static int gridRows = 11;
         private Random random = new Random();
-        private int gridColumns = 11;
-        private int gridRows = 11;
         private Cell[][] cells;
+        private DispatcherTimer timer = new DispatcherTimer();
+        private Game game = new Game(gridRows, gridColumns);
 
         public MainWindow()
         {
-            // create an array for the coordinates of the cells
-            cells = new Cell[gridRows][];
 
             // loads in XAML elements for usage
             InitializeComponent();
-
-            // Set backgroundcolor
-            grid.Background = Brushes.White;
 
             // Add columns to the grid
             for(int i = 0; i < gridColumns; i++)
@@ -45,90 +42,22 @@ namespace Game_of_Life
                 grid.RowDefinitions.Add(new RowDefinition());
             }
 
-            // iterates for every row in the grid
-            for (int x = 0; x < gridRows; x++)
-            {
-                // create new row for the array
-                cells[x] = new Cell[gridColumns];
-
-                // iterates for every column in the grid
-                for (int y = 0; y < gridColumns; y++)
-                {
-                    if(random.Next(2) == 1)
-                    {
-                        cells[x][y] = new Cell(x, y, true);
-                    } else
-                    {
-                        cells[x][y] = new Cell(x, y, false);
-                    }
-
-                    Border border = new Border();
-                    Ellipse circle = new Ellipse();
-
-                    // add border and circle to the grid children
-                    grid.Children.Add(border);
-                    grid.Children.Add(circle);
-
-                    // set x and y coordinates for the border
-                    border.SetValue(Grid.RowProperty, x);
-                    border.SetValue(Grid.ColumnProperty, y);
-
-                    // set x and y coordinates for the circle
-                    circle.SetValue(Grid.RowProperty, x);
-                    circle.SetValue(Grid.ColumnProperty, y);
-
-                    // cell border color and thickness
-                    border.BorderBrush = Brushes.Black;
-                    border.BorderThickness = new Thickness(1);
-
-                    // margin between circle and cell border
-                    circle.Margin = new Thickness(8);
-
-                    // hands over the circle object to the cell object
-                    cells[x][y].Circle = circle;
-
-                    // fills the circles with color
-                    if (cells[x][y].Condition)
-                    {
-                        circle.Fill = Brushes.Black;
-                    }
-                    else
-                    {
-                        circle.Fill = Brushes.White;
-                    }
-
-                    
-                    
-                } // end of for
-            } // end of for
-
-            DispatcherTimer timer = new DispatcherTimer();
-            timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
-            timer.Tick += new EventHandler(Timer_Tick);
-            timer.Start();
+            game.Grid = grid;
+            game.Cell_Generation();
+            Add_Events();
         } // end of method
 
-        private void Timer_Tick(object sender, EventArgs e)
+        private void Add_Events()
         {
-            // iterates for every row in the grid
-            for (int x = 0; x < gridRows; x++)
-            {
-                // iterates for every column in the grid
-                for (int y = 0; y < gridColumns; y++)
-                {
-                    if (random.Next(2) == 1)
-                    {
-                        cells[x][y].Condition = true;
-                        cells[x][y].Circle.Fill = Brushes.Black;
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 1000);
+            timer.Tick += new EventHandler(game.Game_Tick);
+            timer.Start();
 
-                    }
-                    else
-                    {
-                        cells[x][y].Condition = false;
-                        cells[x][y].Circle.Fill = Brushes.White;
-                    }
-                } // end of for
-            } // end of for
-        } // end of method
+            Button button = start_button;
+            // button.Click += new RoutedEventHandler();
+        }
+
+       
     } // end of class
 } // end of namespace
